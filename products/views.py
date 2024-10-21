76dcbda45566cc51a3f2d71ef704b2044cb63238
Yury Petrov <gderpov@yandex.ru>
2024-10-21 13:56:59 +0500

    1) Добавлена страница категорий товаров (categories.html)
    Детали:
    - Были добавлены стили в categories.html; приведи в порядок карточки с категориями
    - Добавлена библиотека Pillow
    - В модель Category добавлен метод - get_absolute_url для доступа к списку товаров по категории

--- products/views.py
+++ products/views.py
@@ -1,5 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 
 def index(request):
     return render(request, 'products/index.html')
+
+
+def categories(request):
+    categories = Category.main_categories.all()
+
+    data = {
+        "categories": categories,
+    }
+
+    return render(request, "products/categories.html", context=data)
+
+
+def catalog(request, cat_slug):
+    category = get_object_or_404(Category, slug=cat_slug)
+    list_product = category.products.all()  # Используем related_name и Product
+
+    data = {
+        "products": list_product,
+    }
+
+    return render(request, "products/cat_catalog.html", context=data)
